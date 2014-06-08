97bbd78b6d31d70c6dab54bc1f7dffa05640214e
shibatako <koki305@gmail.com>
2014-06-08 19:12:30 +0900

    create test

---
+++ src/sample/NumberFormatChecker.java
@@ -0,0 +1,17 @@
+package sample;
+
+import java.util.logging.Logger;
+
+public class NumberFormatChecker {
+	private static final Logger logger = Logger.getLogger("NumberFormatChecker");
+	private static final String CONVERT_ERROR_MSG = "cannot convert to int";
+
+	public static int toInteger(String str){
+		try{
+			return Integer.valueOf(str).intValue();
+		}catch(NumberFormatException e) {
+			logger.warning(str + CONVERT_ERROR_MSG);
+			throw e;
+		}
+	}
+}
