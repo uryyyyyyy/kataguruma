95a806927d3f4cfd2ef0ff49105ca8ac7bfbc403
shibatako <koki305@gmail.com>
2014-06-08 19:31:07 +0900

    add PersonDTO

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,32 @@
 package sample;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		File file = new File("some");
+		FileReader filereader = new FileReader(file);
+		BufferedReader br = new BufferedReader(filereader);
+		
+		String str = br.readLine();
+		List<PersonDTO> list = new ArrayList<>();
+		  while(str != null){
+		    list.add(method1(str));
+		    str = br.readLine();
+		  }
+		  br.close();
+		  
+	}
+
+	public static PersonDTO method1(String str) {
+		String[] personInfo = str.split(" ");
+		return new PersonDTO(Integer.valueOf(personInfo[0]), Integer.valueOf(personInfo[1]));
+	}
 
 }
