ba797faf64f089d8d5d65069316ffccaf87df17e
shibatako <koki305@gmail.com>
2014-06-08 19:37:00 +0900

    sample data

--- src/sample/Caluculator.java
+++ src/sample/Caluculator.java
@@ -1,5 +1,12 @@
 package sample;
 
+import java.util.List;
+
 public class Caluculator {
+	
+	public static int method(List<PersonDTO> list){
+		return 0;
+		
+	}
 
 }
--- test/sample/CaluculatorTest.java
+++ test/sample/CaluculatorTest.java
@@ -1,6 +1,10 @@
 package sample;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -8,7 +12,15 @@ public class CaluculatorTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		List<PersonDTO> list = new ArrayList<PersonDTO>();
+		list.add(new PersonDTO(166,71));
+		list.add(new PersonDTO(178,84));
+		list.add(new PersonDTO(176,94));
+		list.add(new PersonDTO(174,85));
+		list.add(new PersonDTO(174,65));
+		
+		int moke = Caluculator.method(list);
+		assertThat(moke, is(3));
 	}
 
 }
